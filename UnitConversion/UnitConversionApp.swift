5ab1c692dcabea1a7681192354046932d412abf1
vortana say <sayvortana.itc@gmail.com>
2022-12-29 06:38:22 -0600

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by vsay on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
